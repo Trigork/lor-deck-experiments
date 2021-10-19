04ffb2faf76d479ec1b800808869a474991beb01
unknown <trigork@gmail.com>
2021-10-19 08:49:30 +0200

    Since set json files can be accessed without downloading the whole package, we just request the JSON data instead of downloading it

---
+++ cardcodedb/create-carddb.py
@@ -0,0 +1,57 @@
+import os
+import json
+import requests
+from shutil import copyfile
+import argparse
+
+region_codes = {
+    "Demacia" : "DE",
+    "Freljord" : "FR",
+    "Noxus" : "NX",
+    "PiltoverZaun" : "PZ",
+    "ShadowIsles" : "SI",
+    "Targon" : "MT",
+    "BandleCity" : "BC",
+    "Bilgewater" : "BW",
+    "Shurima" : "SH",
+    "Ionia" : "IO"
+}
+
+def main():
+    parser = argparse.ArgumentParser()
+    parser.add_argument("-l", "--locale", type=str, default="en_us", help="Sets the locale of the DB files downloaded")
+    args = parser.parse_args()
+
+    file_path = os.path.dirname(os.path.realpath(__file__))
+
+    set_data = {}
+    for setno in range(1,999):
+        try:
+            url = f'https://dd.b.pvp.net/latest/set{setno}/{args.locale}/data/set{setno}-{args.locale}.json'
+            resp = requests.get(url=url)
+            data = resp.json()
+            set_data[setno] = data
+        except Exception as e:
+            break
+
+    cardcode_db = {}
+    for set_cards in set_data.values():
+        for card in set_cards:
+            cardcode_db.update({card["cardCode"] : {
+                "name" : card["name"],
+                "region" : card["region"],
+                "region_codes" : [ region_codes[r] for r in card["regionRefs"] ],
+                "cost" : card["cost"],
+                "type" : card["type"].lower(),
+                "supertype" : card["supertype"].lower(),
+                "is_token" : not card["collectible"]
+                }
+            })
+
+    outfile = os.sep.join([file_path, "cardcodedb.json"])
+    with open(outfile, 'w') as of:
+        json.dump(cardcode_db, of, indent=4, sort_keys=True)
+
+
+if __name__ == "__main__":
+    main()
